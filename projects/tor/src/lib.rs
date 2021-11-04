adcbc83baafb6c39439069f1b3044c69344d1e9f
Vishal Menon <vishalmenon.92@gmail.com>
2021-11-04 14:46:33 +0530

    Fixed count equation

--- projects/tor/src/lib.rs
+++ projects/tor/src/lib.rs
@@ -11,6 +11,7 @@ mod tests {
   fn test_tor_hashed_password() {
     // generate an even number that can be divided by count subsequent sections. (Thanks Roman)
     const EXPBIAS: u8 = 6;
+    const PREFIX: &str = "16:";
 
     let expected = "16:AA611955E35C4164600ECDB927F1319E62F208D1827EB93A10DFECB9B2".to_lowercase();
 
@@ -23,7 +24,7 @@ mod tests {
     println!("indicator_char: {:?}", char::from_u32(c as u32));
     println!("indicator_unicode: {:?}", c);
 
-    let count: u64 = (16 + (&15)) << ((c >> 4) + EXPBIAS as u64);
+    let count: u64 = (16 + (c&15)) << ((c >> 4) + EXPBIAS as u64);
 
     println!("count: {:?}", count);
 
@@ -38,7 +39,7 @@ mod tests {
     let hashed_password = hasher.finalize();
     let mut hashed_password_hex = hex::encode(&hashed_password);
     hashed_password_hex.insert_str(0, &salt_hex);
-    hashed_password_hex.insert_str(0, "16:");
+    hashed_password_hex.insert_str(0, PREFIX);
 
     assert_eq!(hashed_password_hex, expected);
   }
